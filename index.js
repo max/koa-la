892c664b20508ad6ea1d375f752c03c03554df1a
Max Schoening <max@max.wtf>
2017-04-30 19:11:18 -0700

    Remove koa-router to simplify dependencies

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const Koa = require('koa');
-const Router = require('koa-router');
 
 const server = new Koa();
-const router = new Router();
 
-router.get('/', async ctx => {
+server.use(async ctx => {
   ctx.body = '🐨 are great!';
 });
-
-server.use(router.routes());
 
 if (!module.parent) {
   server.listen(process.env.PORT || 3000, err => {
